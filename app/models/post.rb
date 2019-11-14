dad5114445de064abc0f52d0203cc472d6b56af5
eclectic-coding <chuck@eclecticsaddlebag.com>
2019-11-14 10:01:57 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,11 +8,12 @@ class Post < ActiveRecord::Base
 
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
-      errors.add(:title, "Title must be in title case")
+      errors.add(:title, 'Title must be in title case')
     end
   end
 
   def make_title_case
     self.title = self.title.titlecase
   end
+
 end
